9eeea86ddac509862bd90b40eb54daf7759257f4
AshleyWoods <35774040+AshleyWoods@users.noreply.github.com>
2020-09-30 14:49:13 -0400

    Added grammar and constants
    Added grammar for parsing regular expressions. Updated some notes in the comments. Added global constants to define sigma and the additional characters that can be in the RegEx.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,15 @@
 ///         - RegEx is the regular expression used to recognize strings
 ///     - To exit: ctrl c
 ///     - To test: cargo test
+/// 
+/// ## Grammar for a Regular Expression:
+///     E -> C|E    //The '|' character is part of the actual definition here
+///     E -> C
+///     C -> SC|S
+///     S -> P*|P+|P
+///     P -> (E)|{E}|L
+///     L -> \w|\d|A
+///     A -> All accepted characters (sigma)
 
 
 use std::io::Write; //for writing to output file and stderr
@@ -20,7 +29,12 @@ use std::fs::File; //for creating output file
 use std::io::stdin; //for reading from stdin
 use std::io::prelude::*; //for reading from stdin
 
+//define SIGMA and additionall acceptable chars for refrence
+const SIGMA: [char; 36] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'];
+const REG_CHARS: [char; 8] = ['|', '{', '}', '(', ')', '*', '+', '\\'];
+
 fn main() {
+
     //Grab input from command line
     let mut args = Vec::new();
     for input in std::env::args().skip(1) {
@@ -29,14 +43,15 @@ fn main() {
 
     //Get the regular expression
     let reg_ex  = &args[0];
+    
+    //Parse reg_ex: method call, input regex, output ????, if failed parse print error and exit
 
     //Build a state diagram for reg_ex
-    //UUUUUUUUHHHHHHH??????? - Do so in another method, input is regex, return diagram
+    //Do so in another method, input is ???? from parse, return diagram
 
     //Print the state diagram to stdut
     //Also do so in another method input is diagram, no return, it creates the file
     //To create file: let mut output = File::create("stdout.txt").expect("Unable to create file");
-    
  
     //Read from stdin and print to stderr
     process_input(reg_ex);
