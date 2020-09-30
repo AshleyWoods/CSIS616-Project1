40467f00263bbfad923ab751a8054c69ef95cb13
AshleyWoods <35774040+AshleyWoods@users.noreply.github.com>
2020-09-30 15:14:45 -0400

    Implemented Error checks
    Added error checking for number of input arguments, characters in the regex, and characters in the input strings.

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ use std::io::stdin; //for reading from stdin
 use std::io::prelude::*; //for reading from stdin
 
 //define SIGMA and additionall acceptable chars for refrence
-const SIGMA: [char; 36] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'];
+const SIGMA: [char; 37] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9', ' '];
 const REG_CHARS: [char; 8] = ['|', '{', '}', '(', ')', '*', '+', '\\'];
 
 fn main() {
@@ -41,9 +41,26 @@ fn main() {
 		args.push(input);
     }
 
+    //Check that there is only one input
+    if args.len() != 1 {
+		writeln!(std::io::stderr(), "Invalid Input").unwrap();
+		std::process::exit(1);
+	}
+
     //Get the regular expression
-    let reg_ex  = &args[0];
-    
+    let reg_ex = &args[0];
+
+    //check that input is valid
+    for char in reg_ex.chars() {
+        if !SIGMA.contains(&char) { //if the character is not in sigma
+            if !REG_CHARS.contains(&char) { //and if the character is not in RegEx chars
+                //Then contains unsupported characters and is invalid
+                writeln!(std::io::stderr(), "Invalid Input").unwrap();
+		        std::process::exit(1);
+            }
+        }
+    }
+
     //Parse reg_ex: method call, input regex, output ????, if failed parse print error and exit
 
     //Build a state diagram for reg_ex
@@ -65,8 +82,19 @@ fn main() {
 fn process_input(reg: &str) {
     let mut stderr = std::io::stderr();
     let stdin = stdin();
-    for line in stdin.lock().lines() {
+    'outer: for line in stdin.lock().lines() {
         let string = line.unwrap();
+        
+        //check to make sure the string only containts symbols in the alphabet
+        for char in string.chars() {
+            if !SIGMA.contains(&char) {
+                //if not, string is rejected
+                writeln!(&mut stderr, "Reject {}", string).unwrap();
+                continue 'outer; //continues the outer for loop to go to next string and skip regex
+            }
+        }
+        
+        //if string chars are valid, make sure it matches the regex
         if true{ //Check to see if it matches the regex here
             writeln!(&mut stderr, "Accept {}", string).unwrap();
         }else {
